18b61c86b8b1d74b3d32492e9972d08df7e2245f
YIYAO <jianyy1990@hotmail.com>
2019-04-16 19:13:14 +0000

    fix the bug that <p> is not show in new box

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,17 +1,77 @@
-const tweetData = {
-    "user": {
-        "name": "Newton",
-        "avatars": {
-            "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-            "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-            "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+const data = [
+    {
+        "user": {
+            "name": "Newton",
+            "avatars": {
+                "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+                "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+                "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+            },
+            "handle": "@SirIsaac"
         },
-        "handle": "@SirIsaac"
+        "content": {
+            "text": "If I have seen further it is by standing on the shoulders of giants"
+        },
+        "created_at": 1461116232227
     },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
+    {
+        "user": {
+            "name": "Descartes",
+            "avatars": {
+                "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+                "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+                "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+            },
+            "handle": "@rd"
+        },
+        "content": {
+            "text": "Je pense , donc je suis"
+        },
+        "created_at": 1461113959088
     },
-    "created_at": 1461116232227
+    {
+        "user": {
+            "name": "Johann von Goethe",
+            "avatars": {
+                "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+                "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+                "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+            },
+            "handle": "@johann49"
+        },
+        "content": {
+            "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+        },
+        "created_at": 1461113796368
+    }
+];
+//// time function
+function timeSince(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
 }
 
 // Test / driver code (temporary)
@@ -25,8 +85,9 @@ function createTweetElement(tweetDATA) {
     let $tweet = $('<article>').addClass("tweet");
     let $header = $('<header>');
     let $img = $('<img>').attr('src', src);///<img src="https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png">
-    let $p = $('p').text(text);           ///<p>If I have seen further it is by standing on the shoulders of giants</p>
-    let $span = $('<span>').text('10 days ago')///<span>10 days ago<span>
+    let $p = $('<p>').text(text);           ///<p>If I have seen further it is by standing on the shoulders of giants</p>
+    console.log($p)
+    let $span = $('<span>').text(timeSince(created_at))///<span>10 days ago<span>
     let $footer = $('<footer>');
     let $userID = $('<span>').addClass('userID').text(name); ///<span> Newton </span>
     let $mrField = $('<span>').addClass('mrField').text(handle); //<span> @SirIsaac </span>
@@ -42,13 +103,15 @@ function createTweetElement(tweetDATA) {
     $($tweet).append($footer)
     return $tweet
 }
+function renderTweets(data) {
+    data.forEach(element => {
+        let data = createTweetElement(element)
+        $('#tweets-container').prepend(data)
+    });
+}
 
 $(document).ready(function () {
-
-    let element = createTweetElement(tweetData)
-    $('#tweets-container').append(element);
-
-
+    renderTweets(data)
 })
 
 
