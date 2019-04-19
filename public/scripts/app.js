029a764600b7fe7f42b992f477c5bdaeebeb7fe1
YIYAO <jianyy1990@hotmail.com>
2019-04-19 00:37:48 +0000

    fix bug ajax get didnt pass the servers

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,50 +1,4 @@
-const data = [
-    {
-        "user": {
-            "name": "Newton",
-            "avatars": {
-                "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-                "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-                "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-            },
-            "handle": "@SirIsaac"
-        },
-        "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-        "created_at": 1461116232227
-    },
-    {
-        "user": {
-            "name": "Descartes",
-            "avatars": {
-                "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-                "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-                "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-            },
-            "handle": "@rd"
-        },
-        "content": {
-            "text": "Je pense , donc je suis"
-        },
-        "created_at": 1461113959088
-    },
-    {
-        "user": {
-            "name": "Johann von Goethe",
-            "avatars": {
-                "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-                "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-                "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-            },
-            "handle": "@johann49"
-        },
-        "content": {
-            "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-        },
-        "created_at": 1461113796368
-    }
-];
+
 //// time function
 function timeSince(date) {
     var seconds = Math.floor((new Date() - date) / 1000);
@@ -105,6 +59,7 @@ function createTweetElement(tweetDATA) {
 //     $('#tweets-container').prepend(element);
 // }
 function renderTweets(data) {
+    console.log(data)
     $('#tweets-container').empty()
     data.forEach(element => {
         let data = createTweetElement(element)
@@ -113,7 +68,9 @@ function renderTweets(data) {
 }
 
 function loadTweets() {
-    $.ajax('/tweets', { method: 'GET' }).then(renderTweets(data))
+    $.get('/tweets', (data, status) => {
+        renderTweets(data)
+    })
 }
 function show_hideCompose() {
     $('#nav-bar button').on('click', (e) => {
@@ -143,29 +100,7 @@ function ajaxPost() {
                 type: "POST",
                 url: url,
                 data: { text },
-            }).then(
-                data.push(
-                    {
-                        "user": {
-                            "name": "ME ",
-                            "avatars": {
-                                "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-                                "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-                                "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-                            },
-                            "handle": "@ME"
-                        },
-                        "content": {
-                            "text": text
-                        },
-                        "created_at": new Date()
-                    }
-
-
-                )
-
-
-            ).then(loadTweets())
+            }).then(loadTweets())
         }
 
     });
@@ -186,6 +121,7 @@ function resetText() {
 // })
 
 $(document).ready(function () {
+    loadTweets()
     ajaxPost();
     show_hideCompose();
     resetText();
