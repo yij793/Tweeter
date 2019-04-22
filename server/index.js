7c8f660e2f33104c5e884a7f0decbf0e7717241a
YIYAO <jianyy1990@hotmail.com>
2019-04-22 06:15:55 +0000

    add register route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 "use strict";
-
 // Basic express setup:
 const PORT = 8081;
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+var cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: ['email', 'user'],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -15,27 +22,22 @@ const Mongo = require("mongodb");
 const { MongoClient } = Mongo;
 const MONGODB_URI = 'mongodb://localhost:27017/tweeter';
 ///connect MongoDB
-let dbInstance;
-MongoClient.connect(MONGODB_URI, (err, db) => {
+let db;
+MongoClient.connect(MONGODB_URI, (err, dbInstance) => {
   if (err) {
     throw err;
   }
   console.log(`Successfully connected to DB: ${MONGODB_URI}`);
-  dbInstance = db;
+  db = dbInstance;
   const DataHelpers = require("./lib/data-helpers.js")(db);
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
-  app.use("/tweets", tweetsRoutes);
+  const regRoutes = require("./routes/register")(DataHelpers);
+  app.use("/tweets", tweetsRoutes)
+  app.use("/register", regRoutes)
 });
 
-
-
-
+/////
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
 
-// app.post('/tweets', function (req, res) {
-//   const { text } = req.body;
-//   console.log(text)
-
-// });
